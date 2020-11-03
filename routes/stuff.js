654fc73c97c03ffef88741a7bcb2903b9b3f936a
S2LF <sdeuhelf@gmail.com>
2020-11-03 12:49:03 +0100

    Node JS CRUD + FileSystem on createThing + Auth middleware

---
+++ routes/stuff.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const router = express.Router();
+
+
+const stuffCtrl = require('../controllers/stuff')
+const auth = require('../middleware/auth')
+const multer = require('../middleware/multer-config')
+
+router.post('/', auth, multer, stuffCtrl.createThing)
+router.delete('/:id', auth, stuffCtrl.deleteThing)
+router.put('/:id', auth, multer, stuffCtrl.modifyThing )
+router.get('/:id', auth, stuffCtrl.getOneThing)
+router.get('/', auth, stuffCtrl.getAllThings)
+
+module.exports = router
